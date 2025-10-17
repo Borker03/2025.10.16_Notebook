dde0b03c9bd35f1e5d41fad93e45aa8cd32987cf
Chou bovan <13818431971@163.com>
2025-10-17 11:24:10 +0800

    FIRST STEP
    这是python_2025.10.17部分的第一课的所有内容
    This is all the content of the first lesson in the Python_2025.10.17 section.

---
+++ 第一课.py
@@ -0,0 +1,74 @@
+#第一部分
+#变量=数字
+a = 5
+#变量=文字（字符串str）
+a = "5"
+a = '5'
+#变量=外部输入
+a = input("提示内容（不显示）")
+#转整数
+a = int()
+#转字符串
+a = str()
+#转浮点数（带小数）
+a = float()
+#输出
+print（）
+#多语句同行，分号；分隔
+a=int(input("输入一个整数"))；print（a）
+
+#第二部分:数值计算
+#加减乘除（+-*/）
+#取整
+X//Y=
+#取余
+X%Y=X-Y*（X//Y）
+#指数
+X**Y=pow（X,Y）
+#绝对值
+abs(X)=
+#四舍五入保留N位小数,不写为四舍五入取整数
+round(X,N)=   round(X)
+#取最大，取最小
+max(a,b,c,d)=
+min(a,b,c,d)=
+#保留整数
+int(a)=
+float(a)=#若a为整数则取a.0
+
+#第三部分：字符串
+"a"+"b"="ab"
+"a"*N="N个a"
+#变量="0123456"第一个字符序号为0
+A[a:b]="A[a]-A[b]"
+A[:b]="-A[b]"
+A[a:]="A[a]-"
+A[:]="A"
+str(a)="a"
+A.upper()="A中小写均变为大写"
+A.lower()="A-a"
+len(A)=A的字符个数
+
+#第四部分：print语句
+print("文字")=文字
+print（变量或式）=变量或结果
+print(A,B,C,D,sep="1"，end="2")=A1B1C1D2 #没写sep则sep="",没写end则end="\n"
+
+#换行
+\n
+#整齐
+\t
+print输出默认同行
+
+#第五部分:小知识点
+1.变量名称只能包含字母，汉字，数字，下划线
+2.开头不能是数字
+3.不能包含空格
+4.不能写函数功能的单词做变量名
+#if elif else and or not in while for break
+#continue class def return
+#False True
+#as assert del try except
+#finally from global import is lambda nonlocal
+#pass raise with yield None
+
